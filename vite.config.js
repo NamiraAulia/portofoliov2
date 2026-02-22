a76ef3a4b23d101f535609ac62b649be4887b624
DESKTOP-782F3PO\HP <developer.intikom>
2026-02-22 22:19:59 +0700

    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio-namira/', // Tambahkan ini
+  base: '/portofoliov2/', // Tambahkan ini
 })
